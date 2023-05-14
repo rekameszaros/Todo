f870e69a300257c329b2690e333e1a96e9889ec9
rekameszaros <rikkemeszaros@gmail.com>
2023-05-14 14:28:24 +0200

    more design changes

--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,14 @@ import { Container, Stack, Image } from "@chakra-ui/react";
 import AddTodo from "../components/AddTodo";
 import Auth from "../components/Auth";
 import TodoList from "../components/TodoList";
-
-import listImage from "../public/list.png";
+import gifImage from "../public/listtwo.gif";
+//import listImage from "../public/list.png";
 
 export default function Home() {
   return (
     <Container height={"100vh"} maxW="7xl" padding={"8"}>
       <Stack direction="row" align="center" flex="1">
-        <Image src={listImage.src} alt="List Image" w="400px" h="auto" />
+        <Image src={gifImage.src} alt="List Image" w="400px" h="auto" />
         <AddTodo />
       </Stack>
       <Auth />
