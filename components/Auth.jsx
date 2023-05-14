1a5ae98216f67f12094fc52c4f8280e55e9ff44f
rekameszaros <rikkemeszaros@gmail.com>
2023-05-14 14:39:17 +0200

    Adding comments

--- components/Auth.jsx
+++ components/Auth.jsx
@@ -44,7 +44,7 @@ const Auth = () => {
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider)
       .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
+        // This gives a Google Access Token --- access the Google API.
         const credential = GoogleAuthProvider.credentialFromResult(result);
         const token = credential.accessToken;
         // The signed-in user info.
