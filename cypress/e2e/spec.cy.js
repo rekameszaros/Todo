c630ee6e427be8f09997bf3c1a4f1445468dbc2a
rekameszaros <rikkemeszaros@gmail.com>
2023-05-18 09:56:31 +0200

    test files

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,11 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://todo-jet-seven.vercel.app/')
+  })
+  // it('Does not do much!', () => {
+  //   expect(true).to.equal(true)
+  // })
+  // it('Does not do much!', () => {
+  //   expect(true).to.equal(false)
+  // })
+})
